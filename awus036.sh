3281691e75278203ab54a5d138203b509eff43da
N1P0STK4 <90577203+N1P0STK4@users.noreply.github.com>
2023-02-05 19:39:42 +0100

    Upload awus036.sh

---
+++ awus036.sh
@@ -0,0 +1,18 @@
+#!/bin/bash/
+
+whoami_id=$(id | cut -d "(" -f1 | cut -d "=" -f2)
+
+if [ "$(echo $whoami_id)" == "0" ] ;then
+	apt install realtek-rtl88xxau-dkms -y
+	apt install dkms
+	git clone https://github.com/aircrack-ng/rtl8812au
+	cd rtl8812au
+	make
+	sudo make install
+
+	printf "\e[1;31mDebes de desconectar y volver a conectar la interfaz\e[0m\n"
+	printf "\e[1;32mDebes de desconectar y volver a conectar la interfaz\e[0m\n"
+	printf "\e[1;31mDebes de desconectar y volver a conectar la interfaz\e[0m"
+else
+	printf "Debes de ejecutar el script con privilegios root"
+fi
